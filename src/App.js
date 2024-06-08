f2e4bada9ed2f8ab05422eb3c03cf893864fb444
Akshansh Singh <akshanshsingh478@gmail.com>
2024-06-08 20:58:14 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {        
-    const planets = [
-        { name: 'Earth', isGasPlanet: false },
-        { name: 'Jupiter', isGasPlanet: true },
-        { name: 'Mars', isGasPlanet: false },
-        { name: 'Saturn', isGasPlanet: true },
-        { name: 'Uranus', isGasPlanet: true },
-    ];
+    const [count,setCount] = useState(0);
+    const increaseCount = () => {
+        setCount(count+1);
+    }
+    const decreaseCount = () => {
+        setCount(count-1);
+    }
+    const setToZero = () => {
+        setCount(0);
+    }    
     return (
         <div className='App'>
-            {planets.map((planet, index)=>{
-                if(planet.isGasPlanet) return <h1>{planet.name}</h1>
-            })}
+            <button onClick={increaseCount}>Increase</button>
+            <button onClick={decreaseCount}>Decrease</button>
+            <button onClick={setToZero}>Set to Zero</button>
+            {count}
         </div>
     );
 }
-
-const Job = (props) => {
-    return (
-        <div>
-            <h1>{props.salary}</h1>
-            <h1>{props.position}</h1>
-            <h1>{props.company}</h1>
-        </div>
-    );
-};
 
 export default App;
